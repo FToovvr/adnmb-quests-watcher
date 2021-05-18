97ec515fc48a0811d6729454dfd6c97c6b1d2e99
FToovvr <FToovvr@protonmail.com>
2021-05-18 11:47:03 +0800

    build: 更新格式版本号 3.0 -> 3.1

--- 3_generate_text_report.py
+++ 3_generate_text_report.py
@@ -33,7 +33,7 @@ from models.publication_record import PublicationRecord
 from commons.debugging import super_huge_thread_pg
 from fun.generate_wordcloud import generate_wordcloud
 
-FORMAT_VERSION = '3.0'
+FORMAT_VERSION = '3.1'
 CHARACTER_COUNT_METHOD_VERSION = '2'
 CHARACTER_COUNT_METHOD_EXPLANATION = "除换行与一般空白外字符的个数"
 
